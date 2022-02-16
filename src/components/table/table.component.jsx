ca4e86ad4e2d6c5ad86d55601990fbe3d396b8c1
crosgar <cecilia.rossi.garcia@gmail.com>
2022-02-15 17:57:52 -0800

    put data in a table, added initial delete functionality

---
+++ src/components/table/table.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./table.styles.scss";
+import Button from '@mui/material/Button';
+
+const Table = (props) => {
+   
+   return (
+      <table className="table">
+         <thead>
+            <tr className="table-header-row">
+               {props.thead.map(heading => <th key={heading}>{heading}</th>)}
+            </tr>
+         </thead>
+         <tbody>
+            {props.tbody.map(data => 
+               <tr key={data.id} className="table-data-row">
+                  <td>{data.name}</td>
+                  <td>{data.house}</td>
+                  <td>{data.ancestry}</td>
+                  <td>{data.patronus}</td>
+                  <td><Button variant="outlined" color="error" onClick={() => props.handleDelete(data.id)}>delete</Button></td>
+               </tr> 
+            )}
+         </tbody>
+      </table>
+   )
+}
+
+export default Table;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,21 +1,39 @@
 import React from "react";
 import data from "../../assets/data";
+import Table from "../../components/table/table.component";
 import "./homepage.styles.scss";
 
 class Homepage extends React.Component {
    constructor(props) {
       super(props);
 
-      this.state = {}
+      this.state = {
+         tableData: []
+      }
+   }
+
+   //if data was not hardcoded this is where you could make a fetch / axios.get
+   componentDidMount() {
+      this.setState({tableData: data})
+   }
+
+   //axios.delete
+   handleDelete = (id) => {
+      
+      let newTableData = this.state.tableData.filter(person => person.id !== id);
+      this.setState({tableData: newTableData});
+
    }
 
    render() {
       return (
-         <section>
-            {data.map(character => {
-               return <p key={character.id}>{character.name}</p>
-            })}
-         </section>
+         <div className="homepage">
+            <Table
+               thead={["Name", "House", "Ancestry", "Patronous"]}
+               tbody={this.state.tableData}
+               handleDelete={this.handleDelete}
+            />
+         </div>
       )
    }
    
